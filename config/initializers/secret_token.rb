d5da86ffb68a7c553b5b3a3d19b957e20293c952
Oscar Bacho <oscar.bacho@gmail.com>
2011-05-23 20:37:27 -0500

    Inicial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shareclub::Application.config.secret_token = '0a00530f622a013e079dc39af89fb65c0a2762824b967da52b72b7ce179012eb3a4c4ae107ed9e1451181ef9142f3046292e9ac085c816deeab070f945eec262'
